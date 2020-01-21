c4befc574b831571d078bce7e923c7499409b332
Przemyslaw Leksa <38156110+gnoosny@users.noreply.github.com>
2020-01-21 22:07:35 +0100

    more setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,23 @@
+const path = require('path')
+
+const rules = [
+    {
+        test: /\.tsx?/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+    }
+]
+
 module.exports = {
     target: 'web',
     mode: 'development',
     entry: './src/index.tsx',
     output: {
-        path: null,
+        path: path.resolve(__dirname, 'build'),
         filename: 'bundle.js'
     },
-    module: null,
-    resolve: {extentions: ['.ts', '.tsx', '.js']},
+    module: { rules },
+    resolve: {extensions: ['.ts', '.tsx', '.js']},
     devServer: {
         contentBase: './',
         port: 5000
