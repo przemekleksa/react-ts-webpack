ecf4d0eb1a1357dd864ae3a0d16ecdddf3dea514
Przemyslaw Leksa <38156110+gnoosny@users.noreply.github.com>
2020-01-26 15:42:53 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -17,21 +17,11 @@ export default function App(): JSX.Element {
             getText(value)
             setValue('')
         }
-
-        // const addTodo = (text:string) => {
-        //     text = text.replace(/\r?\n/g, '<newLine>')
-        //     const newTodos: ITodo[] = [ ...todos, {text, complete: false}]
-        //     setTodos(newTodos)
-        // }
-       // console.log(todos)
         const getText = (text: string) =>{
             const data = document.querySelector('.edit')
-            // console.log(data)
-            // data.innerHTML = `<ul><li>${text}</li></ul>`
             text = data.innerHTML
             const newTodos: ITodo[] = [ ...todos, {text, complete: false}]
             setTodos(newTodos)
-            // console.log(text)
         }
         console.log(todos)
         
@@ -41,82 +31,38 @@ export default function App(): JSX.Element {
             <h1>Todo list</h1>
             <form onSubmit={handleSubmit}>
                 <div className="form-group todo-form">
-                    {/* <input type="text" value={value} onChange={e => setValue(e.target.value)} required /> */}
-                    {/* <textarea name="todo" id="" cols="50" rows="3" value={value} onChange={e => setValue(e.target.value)} className="form-control"></textarea> */}
                     <div contentEditable={true} className='form-control edit' ></div>
                     <button type="submit" className="btn btn-primary">Add Todo</button>
                 </div>
             </form>
             <section className="todo-list">
                 {todos.map((todo: ITodo, index: number) => {
-                //    let text = todo.text.split('<newLine>')
-                // let text = todo.text
-                    // return <div key={index}></div> 
-                    
-                   let text = todo.text.replace(/<div>/gi, '!@#').replace(/<\/div>/gi, '!@#').split('!@#') 
-                   return text.map(item =>{
-                    // console.log(item.substring(3,item.length-4))
-                    //     let bText = item.substring(3,item.length-4)
-                        // console.log(item.indexOf('<b>'))
-
+                   let text = todo.text.replace(/<div>/gi, '!@#<div>!@#').replace(/<\/div>/gi, '!@#</div>!@#').split('!@#') 
+                   console.log(text, 'text')
+                   return text.forEach(item =>{
                         let newItem = item.replace(/<b>/gi, '!@#<b>!@#').replace(/<\/b>/gi, '!@#</b>!@#').split('!@#')
-                        console.log(newItem)
-                        let html = ''
-                        let bText = item.substring(item.indexOf('<b>'), item.length)
-                        // console.log(item, 'item')
+                        newItem.filter(el =>{
+                            return el != ""
+                        })
+                        console.log(newItem, 'newItem')
+                        let html = `<div key=${index}>`
                         for(let i = 0; i < newItem.length; i++){
                             if(newItem[i] == '<b>'){
-                                // return <b>{newItem[i+1]}</b>
                                 html += `<b>${newItem[i+1]}</b>`
-                                i += 3
-                            } else if (newItem[i] != '<b>' || newItem[i] != '</b>'){
-                                html +=`${newItem}`
+                                i += 2
+                            }  else if (newItem[i] != '<b>' || newItem[i] != '</b>'){
+                                html +=`${newItem[i]}`
                             }
-                            // if(newItem[i] == '</b>'){
-                            //     html += ''
-                            // }
-                            // if(newItem[i] != '<b>' && newItem[i] != '</b>'){
-                            //     // return newItem[i]
-                            //     html += newItem[i]
-                            // }
-                            // return <div>{html}</div>
-                            
-                            // } else if (newItem[i] === '</b>'){
-                                
-                            // } else
-                            // {
-                            //     return newItem[i]
-                            // }
                         }
-                        html = html.replace(',','')
-                        console.log(html)
+                        html += `</div>`
+                        console.log(html, 'html')
                         document.querySelector('.todo-list').innerHTML += html
-                        // if(item.includes('<b>')){
-                        //     return <b>{bText}</b>
-                        // } else return <div>{item}</div>
-                    //    if(item.substring(0,3) === '<b>'){
-                    //        return <b>{bText}</b>
-                    //    } else return <div>{item}</div>
-                    // return item
+                        html =''
                    })
-                //    console.log(text)
-                    // return <div key={index} id={index} className='text'> {text.map((item: string, index: number) => {
-                    //         return <div key={index}>{item}</div>
-                    //         })}
-                    //         </div>
-                    
-                //    if(text[text.length] == '') {text = text.slice(0, -1)}
-                //    return <div key={index}>{text}</div>
-                    // return <div key={index} id={index}> {text.map((item: string, index: number) => {
-                    //     return <div key={index}>
-                    //     {item}
-                    //     </div> })} </div>
-                    // return <div key={index} id={index}> {text.map((item: string, index: number) => {
-                    //     return <div key={index}>{item}</div> })} </div>
-                })}
-                {
-                    
                 }
+                )}
+               
+              
                 
             </section>
            
